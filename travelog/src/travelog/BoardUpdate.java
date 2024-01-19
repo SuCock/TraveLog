f857a5965076f79ae1b801e75d7a9dfba55fecf4
SuCock <ksymin313@gmail.com>
2024-01-19 18:12:13 +0900

    240119 수정전에 조회

---
+++ travelog/src/travelog/BoardUpdate.java
@@ -0,0 +1,38 @@
+package travelog;
+
+import java.util.Scanner;
+
+public class BoardUpdate {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		DB db = new DB();
+		
+		while (true) {
+			System.out.println("1. 게시글 단건 조회");
+			System.out.println("2. 게시글 수정");
+			System.out.println("3. 종료");
+			System.out.println("번호 입력하세요 > ");
+
+			String board = sc.nextLine();
+
+			if (board.equals("1")) {
+				System.out.println("게시글 번호 입력 > ");
+				db.select(board);
+			} else if (board.equals("2")) {
+				System.out.println("게시글 제목 > ");
+				String input = sc.nextLine();
+				db.update(board); // 제목
+				System.out.println("게시글 내용 > ");
+				input = sc.nextLine();
+				db.update(board); // 내용
+				System.out.println("게시글 번호 > ");
+				input = sc.nextLine();
+				db.update(board); // 게시글 번호
+				db.select(board);
+			} else {
+				System.out.println("종료합니다.");
+				break;
+			}
+		}
+	}
+}
--- travelog/src/travelog/DB.java
+++ travelog/src/travelog/DB.java
@@ -6,36 +6,46 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DB {
-	public void insert(String todo) {
-		System.out.println("DB에 할일 입력하기");
-		// 예외 처리
+	public void select(String board) { // 조회문
+		System.out.println("");
 		try {
 			Connection con = DriverManager.getConnection(INFO.JDBC_URL, INFO.USERNAME, INFO.PASSWORD);
-			PreparedStatement pstmt = con.prepareStatement("INSERT INTO todo "
-														 + "(`할일`,`상태`)"
-														 + "VALUES "
-														 + "(?,'준비')");
-			pstmt.setString(1, todo); // 첫번째 ?에 넣어준다.
-			System.out.println("입력완료");
-			pstmt.executeUpdate(); // 입력한 퀴리문을 업데이트 해준다.
+			PreparedStatement pstmt = con.prepareStatement("SELECT board_category\r\n"
+														 + "	 , board_writer\r\n"
+													     + "	 , board_date\r\n"
+													     + "	 , board_title\r\n"
+													     + "	 , board_contents\r\n"
+													     + "  FROM board\r\n"
+													     + " WHERE board_no = ?");
+			pstmt.setString(1, board); // 첫번째 ?에 넣어준다. -> 조건을 먼저 넣고 실행해야한다.
+			ResultSet rs = pstmt.executeQuery(); // 반환값을 넣을곳이 필요하다.	
+			if (rs.next()) { // 데이터가 있는 경우에만 진행
+				System.out.println("╔══════" + rs.getString("board_title") + "═╗");
+		        System.out.println("║    " + rs.getString("board_category") + "║");
+		        System.out.println("║      " + rs.getString("board_writer") + "║");
+		        System.out.println("║        " + rs.getString("board_date") + "║");
+		        System.out.println("║    " + rs.getString("board_contents") + "║");
+		        System.out.println("╚══════════════════════════════════════════╝");
+			}
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	public void select() {
-		System.out.println("DB에 할일 조회하기");
+
+	public void update(String board) { // 수정문
+		System.out.println("DB에 할일 입력하기");
+		// 예외 처리
 		try {
 			Connection con = DriverManager.getConnection(INFO.JDBC_URL, INFO.USERNAME, INFO.PASSWORD);
-			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM todo"); 
-			ResultSet rs = pstmt.executeQuery(); // 반환값을 넣을곳이 필요하다.
-			
-			while(rs.next()) {
-				System.out.print("idx: " + rs.getInt("idx"));
-				System.out.print(" 할일: " + rs.getString("할일"));
-				System.out.println(" 상태: " + rs.getString("상태"));
-			}
-			System.out.println("조회완료");
-		}catch (Exception e) {
+			PreparedStatement pstmt = con.prepareStatement("UPDATE travelog\r\n"
+															+ "SET board_title = '?'\r\n"
+															+ "  , board_contnets = '?'\r\n"
+														  + "WHERE board_no = '?';");
+			pstmt.setString(1, board); // 첫번째 ?에 넣어준다.
+			pstmt.setString(2, board); // 두번째 ?에 넣어준다.
+			pstmt.setString(3, board); // 세번째 ?에 넣어준다.
+			pstmt.executeUpdate(); // 입력한 퀴리문을 업데이트 해준다.
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
