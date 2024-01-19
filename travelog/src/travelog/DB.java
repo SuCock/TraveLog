2025d56e1c7acc2a161b7c0f15b7e880b67908bf
SuCock <ksymin313@gmail.com>
2024-01-19 14:34:05 +0900

    기본설정

---
+++ travelog/src/travelog/DB.java
@@ -0,0 +1,42 @@
+package travelog;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DB {
+	public void insert(String todo) {
+		System.out.println("DB에 할일 입력하기");
+		// 예외 처리
+		try {
+			Connection con = DriverManager.getConnection(INFO.JDBC_URL, INFO.USERNAME, INFO.PASSWORD);
+			PreparedStatement pstmt = con.prepareStatement("INSERT INTO todo "
+														 + "(`할일`,`상태`)"
+														 + "VALUES "
+														 + "(?,'준비')");
+			pstmt.setString(1, todo); // 첫번째 ?에 넣어준다.
+			System.out.println("입력완료");
+			pstmt.executeUpdate(); // 입력한 퀴리문을 업데이트 해준다.
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void select() {
+		System.out.println("DB에 할일 조회하기");
+		try {
+			Connection con = DriverManager.getConnection(INFO.JDBC_URL, INFO.USERNAME, INFO.PASSWORD);
+			PreparedStatement pstmt = con.prepareStatement("SELECT * FROM todo"); 
+			ResultSet rs = pstmt.executeQuery(); // 반환값을 넣을곳이 필요하다.
+			
+			while(rs.next()) {
+				System.out.print("idx: " + rs.getInt("idx"));
+				System.out.print(" 할일: " + rs.getString("할일"));
+				System.out.println(" 상태: " + rs.getString("상태"));
+			}
+			System.out.println("조회완료");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
