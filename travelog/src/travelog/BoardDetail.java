2b6bd01b25bd6d11fc1e40f1d0a8ca19f8190669
KimSang-Ui <k93158612@gmail.com>
2024-01-24 18:11:07 +0900

    상세조회 콘솔만드는 중

---
+++ travelog/src/travelog/BoardDetail.java
@@ -0,0 +1,18 @@
+package travelog;
+
+import java.util.Scanner;
+
+public class BoardDetail {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		DB db = new DB();
+		
+		while(true) {
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+}
