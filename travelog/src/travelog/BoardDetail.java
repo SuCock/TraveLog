f574d0f171fe56f92bbea6014d9da63180f1d70a
SuCock <ksymin313@gmail.com>
2024-01-29 17:41:52 +0900

    no message

--- travelog/src/travelog/BoardDetail.java
+++ travelog/src/travelog/BoardDetail.java
@@ -3,9 +3,12 @@ package travelog;
 import java.util.Scanner;
 
 public class BoardDetail {
+	
+	public static DB db = new DB();		
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		DB db = new DB();				
+				
 		int board_no;
 		while(true) {
 			System.out.println("게시물을 선택하세요.");
@@ -30,7 +33,7 @@ public class BoardDetail {
                 // 게시글 수정
                 break;
             case 3:
-                db.Boarddelete.main(args);
+//                db.Boarddelete.main(args);
                 // 게시글삭제
                 break;
             case 4:
@@ -39,42 +42,46 @@ public class BoardDetail {
                 return;  // 프로그램 종료
             default:
                 System.out.println("알맞은 번호를 입력하세요.");
+	        }
         }
     }
 
 	private static boolean isValidBoard_no(String Board_no) {
 		// 유효한 게시물 번호인지 확인하는 메서드
-		return Board_no.equals("1.경상도") || Board_no.equals("2.충청도") || Board_no.equals("3.전라도") || Board_no.equals("4.강원도")
+		return Board_no.equals("1.경상도")
+				|| Board_no.equals("2.충청도")
+				|| Board_no.equals("3.전라도") 
+				|| Board_no.equals("4.강원도")
 				|| Board_no.equals("5.경기도");
- 
-		Scanner sc = new Scanner(System.in);
-
-		while (true) {
-			int boardNo = 1; // 게시물 내용 출력할 수 있게 하는 메서드
-			db.updateSelect(board_no); // 단건조회
-			System.out.println("1. 게시물 수정");
-			System.out.println("2. 게시물 삭제");
-			System.out.println("3. 메인페이지로 이동");
-			System.out.println("번호 입력하세요 > ");
-
-			String number = sc.nextLine();
-			if (number.equals("1")) {
-				System.out.println("게시물 작성날짜 > ");
-				String date = sc.nextLine(); // 작성날짜 작성
-				System.out.println("게시물 제목 > ");
-				String title = sc.nextLine(); // 제목 작성
-				System.out.println("게시물 내용 > ");
-				String contents = sc.nextLine(); // 내용 작성
-				System.out.println("게시물 작성자");
-				String writer = sc.nextLine();// 작성자 작성
-
-				db.insert(date, title, contents, writer); // 입력한 게시판 번호
-			} else {
-				System.out.println("종료합니다.");
-				break;
-			}
-		}
 	}
+ 
+//		Scanner sc = new Scanner(System.in);
+//
+//		while (true) {
+//			int boardNo = 1; // 게시물 내용 출력할 수 있게 하는 메서드
+//			db.updateSelect(boardNo); // 단건조회
+//			System.out.println("1. 게시물 수정");
+//			System.out.println("2. 게시물 삭제");
+//			System.out.println("3. 메인페이지로 이동");
+//			System.out.println("번호 입력하세요 > ");
+//
+//			String number = sc.nextLine();
+//			if (number.equals("1")) {
+//				System.out.println("게시물 작성날짜 > ");
+//				String date = sc.nextLine(); // 작성날짜 작성
+//				System.out.println("게시물 제목 > ");
+//				String title = sc.nextLine(); // 제목 작성
+//				System.out.println("게시물 내용 > ");
+//				String contents = sc.nextLine(); // 내용 작성
+//				System.out.println("게시물 작성자");
+//				String writer = sc.nextLine();// 작성자 작성
+//
+//				db.insert(date, title, contents, writer); // 입력한 게시판 번호
+//			} else {
+//				System.out.println("종료합니다.");
+//				break;
+//			}
+//		}
 }
 
 
