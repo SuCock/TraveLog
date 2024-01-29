5f5ac9c78be1be3eb7fbb5ec6e22d8c04828db24
KimSang-Ui <k93158612@gmail.com>
2024-01-29 17:49:05 +0900

    no message

--- travelog/src/travelog/BoardDetail.java
+++ travelog/src/travelog/BoardDetail.java
@@ -5,45 +5,43 @@ import java.util.Scanner;
 public class BoardDetail {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		DB db = new DB();				
+		DB db = new DB();
 		int board_no;
-		while(true) {
+		while (true) {
 			System.out.println("게시물을 선택하세요.");
 			System.out.println("게시물 번호 입력 > (1.경상도 2.충청도 3.전라도 4.강원도 5.경기도");
-			  
+
 			int choice = sc.nextInt();
-	        sc.nextLine();
-	        
-	        switch(choice) {
-            case 1:
-                boolean check = false;
-                do {
-                    System.out.println("게시물 번호를 입력하세요");
-                    System.out.println("1.경상도 2.충청도 3.전라도 4.강원도 5.경기도");
-                    board_no = sc.nextInt();
-                    
-                } while (!check); // false 가 되면 종료 - true 이면 계속 실행
-                db.detailSelect(board_no);
-                break;
-            case 2:
-                BoardUpdate.main(args);
-                // 게시글 수정
-                break;
-            case 3:
-                db.Boarddelete.main(args);
-                // 게시글삭제
-                break;
-            case 4:
-                // 메인화면이동
-                System.out.println("메인 화면으로 이동합니다.");
-                return;  // 프로그램 종료
-            default:
-                System.out.println("알맞은 번호를 입력하세요.");
-        }
-    }
+			sc.nextLine();
 
+			switch (choice) {
+			case 1:
+				boolean check = false;
+				do {
+					System.out.println("게시물 번호를 입력하세요");
+					System.out.println("1.경상도 2.충청도 3.전라도 4.강원도 5.경기도");
+					board_no = sc.nextInt();
 
-
+				} while (!check); // false 가 되면 종료 - true 이면 계속 실행
+				db.detailSelect(board_no);
+				break;
+			case 2:
+				BoardUpdate.main(args);
+				// 게시글 수정
+				break;
+			case 3:
+                Boarddelete.main(args);
+				// 게시글삭제
+				break;
+			case 4:
+				// 메인화면이동
+				System.out.println("메인 화면으로 이동합니다.");
+				return; // 프로그램 종료
+			default:
+				System.out.println("알맞은 번호를 입력하세요.");
+			}
+		}
+		
 		while (true) {
 			int boardNo = 1; // 게시물 내용 출력할 수 있게 하는 메서드
 			db.updateSelect(board_no); // 단건조회
@@ -69,7 +67,6 @@ public class BoardDetail {
 				break;
 			}
 		}
+
 	}
 }
-
-
