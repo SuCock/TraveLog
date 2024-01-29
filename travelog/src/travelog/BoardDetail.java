91c7a8627f9fba1e032714bec63971a0375e44a6
KimSang-Ui <k93158612@gmail.com>
2024-01-30 01:17:14 +0900

    .

--- travelog/src/travelog/BoardDetail.java
+++ travelog/src/travelog/BoardDetail.java
@@ -7,18 +7,24 @@ public class BoardDetail {
 	public static void main(String[] args) {
 		DB db = new DB();
 		Scanner sc = new Scanner(System.in);
+		System.out.println("게시물 번호를 입력하세요.");
+		
+		int choice = sc.nextInt();
+        sc.nextLine();
 
-		while (true) {
-			System.out.println("게시물 번호를 입력하세요.");
-			int board_no = sc.nextInt();
-			 String board_contents = db.retrieve(board_no);
-
-	            if (board_contents != null) {
-	                System.out.println("게시물 내용:");
-	                System.out.println(board_contents);
-	            } else {
-	                System.out.println("해당 번호의 게시물이 존재하지 않습니다.");
-	            }
-	        }
-	    }
+		switch (choice) {
+		case 1:
+			BoardUpdate.main(args);
+			break;
+		case 2:
+			db.delete(choice);		
+			break;
+		case 3:
+			System.out.println("메인화면으로 돌아갑니다.");
+			BoardDetail.main(args);
+			return;
+		default:
+			System.out.println("잘못된 번호를 입력했습니다.");
+		}
 	}
+}
