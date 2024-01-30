6afe3c7fa08ae124fecc3d8af43a76507cdaf3b6
KimSang-Ui <k93158612@gmail.com>
2024-01-30 16:35:38 +0900

    no message

--- travelog/src/travelog/BoardDetail.java
+++ travelog/src/travelog/BoardDetail.java
@@ -12,7 +12,7 @@ public class BoardDetail {
 		int choice = sc.nextInt();
 		sc.nextLine();
         db.detailSelect(choice);
-        
+        System.out.println();
         System.out.println("게시물 수정,  게시물 삭제, 메인화면으로 이동");
 
 	}
