0d1852c8b1c6dcdf2ee563b42850e78bb266de11
rxxrxxr <wnf2255@gmail.com>
2024-01-31 17:25:50 +0900

    20240131

--- travelog/src/travelog/BoardDetail.java
+++ travelog/src/travelog/BoardDetail.java
@@ -8,12 +8,10 @@ public class BoardDetail {
 		DB db = new DB();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("게시물 번호를 입력하세요.");
-		
 		int choice = sc.nextInt();
 		sc.nextLine();
-        db.detailSelect(choice);
-        System.out.println();
-        System.out.println("1. 게시물 수정, 2. 게시물 삭제, 3.메인화면으로 이동");
-
+		db.detailSelect(choice);
+		System.out.println();
+		System.out.println("1. 게시물 수정, 2. 게시물 삭제, 3.메인화면으로 이동");
 	}
 }
