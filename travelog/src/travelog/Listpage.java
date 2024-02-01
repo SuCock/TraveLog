d5ea8bde770d6a36c8d96032f602480ebba5c949
rxxrxxr <wnf2255@gmail.com>
2024-02-01 18:09:35 +0900

    2024 0201 18:10 고찬우

--- travelog/src/travelog/Listpage.java
+++ travelog/src/travelog/Listpage.java
@@ -24,15 +24,15 @@ public class Listpage {
             	boolean check = false;
                 String cate;
                 do {
-        
                     System.out.println("카테고리를 입력해주세요.");
                     System.out.println("경상도 충청도 전라도 강원도 경기도");
                     cate = scan.nextLine();
                     if (isValidCategory(cate)) {
-                        check = true;
-                    } 
+                        db.cateSelect(cate);
+                    } else {
+                    	System.out.println("다시입력해주세요");
+                    }
                 } while (!check); // false 가 되면 종료 - true면 계속 실행
-                db.cateSelect(cate);
                 break;
             case 2:
             	// 게시글 선택
